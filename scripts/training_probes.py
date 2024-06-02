d6737d2d65874082aca72db0708109078ef0a2bb
Jim Maar <jimmaar1@Gmail.com>
2024-06-02 22:08:45 +0200

    cleanup

--- scripts/training_probes.py
+++ scripts/training_probes.py
@@ -1,7 +1,6 @@
 import os, sys
 chapter = "chapter1_transformer_interp"
 repo = "ARENA_3.0"
-
 chapter_dir = r"./" if chapter in os.listdir() else os.getcwd().split(chapter)[0]
 sys.path.append(chapter_dir + f"{chapter}/exercises")
 
@@ -33,23 +32,11 @@ import dataclasses
 import datasets
 from IPython.display import HTML
 import transformer_lens
-import transformer_lens.utils as utils
+import utils
 from transformer_lens.hook_points import HookedRootModule, HookPoint
 from transformer_lens import HookedTransformer, HookedTransformerConfig, FactoredMatrix, ActivationCache
-from tqdm.notebook import tqdm
-from dataclasses import dataclass
-from rich import print as rprint
-import pandas as pd
 
-# Make sure exercises are in the path
-# exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
-# section_dir = exercises_dir / "part6_othellogpt"
-# section_dir = "interpretability"
-# if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
-
-from plotly_utils import imshow
-from neel_plotly import scatter, line
-import part6_othellogpt.tests as tests
+import wandb
 
 from training_utils import get_state_stack_one_hot_flipped, get_state_stack_one_hot_num_flipped, get_state_stack_one_hot_even_odd_flipped
 
@@ -59,62 +46,15 @@ t.set_grad_enabled(False)
 
 MAIN = __name__ == "__main__"
 
-cfg = HookedTransformerConfig(
-    n_layers = 8,
-    d_model = 512,
-    d_head = 64,
-    n_heads = 8,
-    d_mlp = 2048,
-    d_vocab = 61,
-    n_ctx = 59,
-    act_fn="gelu",
-    normalization_type="LNPre",
-    device=device,
-)
-model = HookedTransformer(cfg)
+model = utils.load_model(device)
 
-sd = utils.download_file_from_hf("NeelNanda/Othello-GPT-Transformer-Lens", "synthetic_model.pth")
-# champion_ship_sd = utils.download_file_from_hf("NeelNanda/Othello-GPT-Transformer-Lens", "championship_model.pth")
-model.load_state_dict(sd)
-
-# An example input
-sample_input = t.tensor([[
-    20, 19, 18, 10,  2,  1, 27,  3, 41, 42, 34, 12,  4, 40, 11, 29, 43, 13, 48, 56,
-    33, 39, 22, 44, 24,  5, 46,  6, 32, 36, 51, 58, 52, 60, 21, 53, 26, 31, 37,  9,
-    25, 38, 23, 50, 45, 17, 47, 28, 35, 30, 54, 16, 59, 49, 57, 14, 15, 55, 7
-]]).to(device)
-
-# The argmax of the output (ie the most likely next move from each position)
-sample_output = t.tensor([[
-    21, 41, 40, 34, 40, 41,  3, 11, 21, 43, 40, 21, 28, 50, 33, 50, 33,  5, 33,  5,
-    52, 46, 14, 46, 14, 47, 38, 57, 36, 50, 38, 15, 28, 26, 28, 59, 50, 28, 14, 28,
-    28, 28, 28, 45, 28, 35, 15, 14, 30, 59, 49, 59, 15, 15, 14, 15,  8,  7,  8
-]]).to(device)
-
-assert (model(sample_input).argmax(dim=-1) == sample_output.to(device)).all()
-
-# os.chdir(section_dir)
 section_dir = Path.cwd()
 assert section_dir.name == "interpretability"
 
 OTHELLO_ROOT = (section_dir / "othello_world").resolve()
 OTHELLO_MECHINT_ROOT = (OTHELLO_ROOT / "mechanistic_interpretability").resolve()
 
-# if not OTHELLO_ROOT.exists():
-#     !git clone https://github.com/likenneth/othello_world
-
 sys.path.append(str(OTHELLO_MECHINT_ROOT))
-
-from mech_interp_othello_utils import (
-    plot_board,
-    plot_single_board,
-    plot_board_log_probs,
-    to_string,
-    to_int,
-    int_to_label,
-    string_to_label,
-    OthelloBoardState
-)
 
 # Load board data as ints (i.e. 0 to 60)
 board_seqs_int = t.tensor(np.load(OTHELLO_MECHINT_ROOT / "board_seqs_int_small.npy"), dtype=t.long)
@@ -123,72 +63,6 @@ board_seqs_string = t.tensor(np.load(OTHELLO_MECHINT_ROOT / "board_seqs_string_s
 
 assert all([middle_sq not in board_seqs_string for middle_sq in [27, 28, 35, 36]])
 assert board_seqs_int.max() == 60
-
-num_games, length_of_game = board_seqs_int.shape
-
-# Define possible indices (excluding the four center squares)
-stoi_indices = [i for i in range(64) if i not in [27, 28, 35, 36]]
-
-# Define our rows, and the function that converts an index into a (row, column) label, e.g. `E2`
-alpha = "ABCDEFGH"
-
-def to_board_label(i):
-    return f"{alpha[i//8]}{i%8}"
-
-# Get our list of board labels
-board_labels = list(map(to_board_label, stoi_indices))
-full_board_labels = list(map(to_board_label, range(64)))
-
-def plot_square_as_board(state, diverging_scale=True, **kwargs):
-    """Takes a square input (8 by 8) and plot it as a board. Can do a stack of boards via facet_col=0"""
-    kwargs = {
-        "y": [i for i in alpha],
-        "x": [str(i) for i in range(8)],
-        "color_continuous_scale": "RdBu" if diverging_scale else "Blues",
-        "color_continuous_midpoint": 0. if diverging_scale else None,
-        "aspect": "equal",
-        **kwargs
-    }
-    imshow(state, **kwargs)
-
-num_games = 50
-focus_games_int = board_seqs_int[:num_games]
-focus_games_string = board_seqs_string[:num_games]
-
-focus_logits, focus_cache = model.run_with_cache(focus_games_int[:, :-1].to(device))
-focus_logits.shape
-
-full_linear_probe = t.load(OTHELLO_MECHINT_ROOT / "main_linear_probe.pth", map_location=device)
-
-rows = 8
-cols = 8
-options = 3
-assert full_linear_probe.shape == (3, cfg.d_model, rows, cols, options)
-
-black_to_play_index = 0
-white_to_play_index = 1
-blank_index = 0
-their_index = 1
-my_index = 2
-
-# Creating values for linear probe (converting the "black/white to play" notation into "me/them to play")
-linear_probe = t.zeros(cfg.d_model, rows, cols, options, device=device)
-linear_probe[..., blank_index] = 0.5 * (full_linear_probe[black_to_play_index, ..., 0] + full_linear_probe[white_to_play_index, ..., 0])
-linear_probe[..., their_index] = 0.5 * (full_linear_probe[black_to_play_index, ..., 1] + full_linear_probe[white_to_play_index, ..., 2])
-linear_probe[..., my_index] = 0.5 * (full_linear_probe[black_to_play_index, ..., 2] + full_linear_probe[white_to_play_index, ..., 1])
-
-layer = 6
-game_index = 0
-move = 29
-
-def plot_probe_outputs(layer, game_index, move, **kwargs):
-    residual_stream = focus_cache["resid_post", layer][game_index, move]
-    # print("residual_stream", residual_stream.shape)
-    probe_out = einops.einsum(residual_stream, linear_probe, "d_model, d_model row col options -> row col options")
-    probabilities = probe_out.softmax(dim=-1)
-    plot_square_as_board(probabilities, facet_col=2, facet_labels=["P(Empty)", "P(Their's)", "P(Mine)"], **kwargs)
-
-import wandb
 
 # We train different Probes on the Model
 # Probe1. Detect if a Tile has been Flipped this turn TODO: Look at the Code from Andrew Lee (e. g. what exactly was the loss)
@@ -250,24 +124,6 @@ class ProbeTrainingArgs():
 
 # set torch seed to 42
 t.manual_seed(42)
-
-'''n_indices = 4
-full_train_indices = t.randperm(4)[:n_indices]
-full_train_indices = einops.rearrange(full_train_indices, "(batch_idx game_idx) -> batch_idx game_idx", game_idx=2)
-indeces = full_train_indices[0]
-games_str = board_seqs_string[indeces.cpu()]
-state_stack = seq_to_state_stack(games_str[0].tolist())
-imshow(
-    state_stack[:16],
-    facet_col=0,
-    facet_col_wrap=8,
-    facet_labels=[f"Move {i}" for i in range(1, 17)],
-    title="First 16 moves of first game",
-    color_continuous_scale="Greys",
-)
-args = ProbeTrainingArgs()
-get_state_stack_one_hot_num_flipped(games_str, args)'''
-
 
 class LinearProbeTrainer:
     def __init__(self, model: HookedTransformer, args: ProbeTrainingArgs):
@@ -388,26 +244,6 @@ class LinearProbeTrainer:
         wandb.finish()
         self.save_linear_probe(f"models/{args.probe_name}.pth")
         print("Probe Trained and Saved")
-
-
-# Train Flipped Probe
-'''for probe in ["flipped", "num_flipped", "even_odd_flipped"]:
-    for layer in range(8):
-        args = ProbeTrainingArgs()
-        args.layer = layer
-        args.wandb_project = "Othello-GPT-Probes"
-        args.probe_name = probe
-        if probe == "flipped":
-            args.get_state_stack_one_hot = get_state_stack_one_hot_flipped
-            args.num_outputs = 2
-        elif probe == "num_flipped":
-            args.get_state_stack_one_hot = get_state_stack_one_hot_num_flipped
-            args.num_outputs = 18
-        elif probe == "even_odd_flipped":
-            args.get_state_stack_one_hot = get_state_stack_one_hot_even_odd_flipped
-            args.num_outputs = 2
-        trainer = LinearProbeTrainer(model, args)
-        trainer.train()'''
 
 if __name__ == "__main__":
     # TODO: Add Probes for the other 5 Probes
--- scripts/training_utils.py
+++ scripts/training_utils.py
@@ -32,23 +32,10 @@ import dataclasses
 import datasets
 from IPython.display import HTML
 import transformer_lens
-import transformer_lens.utils as utils
+import transformer_lens.utils
 from transformer_lens.hook_points import HookedRootModule, HookPoint
 from transformer_lens import HookedTransformer, HookedTransformerConfig, FactoredMatrix, ActivationCache
-from tqdm.notebook import tqdm
-from dataclasses import dataclass
-from rich import print as rprint
-import pandas as pd
-
-# Make sure exercises are in the path
-# exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
-# section_dir = exercises_dir / "part6_othellogpt"
-# section_dir = "interpretability"
-# if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
-
-from plotly_utils import imshow
-from neel_plotly import scatter, line
-import part6_othellogpt.tests as tests
+import utils
 
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
 
@@ -56,39 +43,7 @@ t.set_grad_enabled(False)
 
 MAIN = __name__ == "__main__"
 
-cfg = HookedTransformerConfig(
-    n_layers = 8,
-    d_model = 512,
-    d_head = 64,
-    n_heads = 8,
-    d_mlp = 2048,
-    d_vocab = 61,
-    n_ctx = 59,
-    act_fn="gelu",
-    normalization_type="LNPre",
-    device=device,
-)
-model = HookedTransformer(cfg)
-
-sd = utils.download_file_from_hf("NeelNanda/Othello-GPT-Transformer-Lens", "synthetic_model.pth")
-# champion_ship_sd = utils.download_file_from_hf("NeelNanda/Othello-GPT-Transformer-Lens", "championship_model.pth")
-model.load_state_dict(sd)
-
-# An example input
-sample_input = t.tensor([[
-    20, 19, 18, 10,  2,  1, 27,  3, 41, 42, 34, 12,  4, 40, 11, 29, 43, 13, 48, 56,
-    33, 39, 22, 44, 24,  5, 46,  6, 32, 36, 51, 58, 52, 60, 21, 53, 26, 31, 37,  9,
-    25, 38, 23, 50, 45, 17, 47, 28, 35, 30, 54, 16, 59, 49, 57, 14, 15, 55, 7
-]]).to(device)
-
-# The argmax of the output (ie the most likely next move from each position)
-sample_output = t.tensor([[
-    21, 41, 40, 34, 40, 41,  3, 11, 21, 43, 40, 21, 28, 50, 33, 50, 33,  5, 33,  5,
-    52, 46, 14, 46, 14, 47, 38, 57, 36, 50, 38, 15, 28, 26, 28, 59, 50, 28, 14, 28,
-    28, 28, 28, 45, 28, 35, 15, 14, 30, 59, 49, 59, 15, 15, 14, 15,  8,  7,  8
-]]).to(device)
-
-assert (model(sample_input).argmax(dim=-1) == sample_output.to(device)).all()
+model = utils.load_model(device)
 
 # os.chdir(section_dir)
 section_dir = Path.cwd()
@@ -97,19 +52,9 @@ assert section_dir.name == "interpretability"
 OTHELLO_ROOT = (section_dir / "othello_world").resolve()
 OTHELLO_MECHINT_ROOT = (OTHELLO_ROOT / "mechanistic_interpretability").resolve()
 
-# if not OTHELLO_ROOT.exists():
-#     !git clone https://github.com/likenneth/othello_world
-
 sys.path.append(str(OTHELLO_MECHINT_ROOT))
 
 from mech_interp_othello_utils import (
-    plot_board,
-    plot_single_board,
-    plot_board_log_probs,
-    to_string,
-    to_int,
-    int_to_label,
-    string_to_label,
     OthelloBoardState
 )
 
@@ -121,62 +66,9 @@ board_seqs_string = t.tensor(np.load(OTHELLO_MECHINT_ROOT / "board_seqs_string_s
 assert all([middle_sq not in board_seqs_string for middle_sq in [27, 28, 35, 36]])
 assert board_seqs_int.max() == 60
 
-num_games, length_of_game = board_seqs_int.shape
-
-# Define possible indices (excluding the four center squares)
-stoi_indices = [i for i in range(64) if i not in [27, 28, 35, 36]]
-
-# Define our rows, and the function that converts an index into a (row, column) label, e.g. `E2`
-alpha = "ABCDEFGH"
-
-def to_board_label(i):
-    return f"{alpha[i//8]}{i%8}"
-
-# Get our list of board labels
-board_labels = list(map(to_board_label, stoi_indices))
-full_board_labels = list(map(to_board_label, range(64)))
-
-def plot_square_as_board(state, diverging_scale=True, **kwargs):
-    """Takes a square input (8 by 8) and plot it as a board. Can do a stack of boards via facet_col=0"""
-    kwargs = {
-        "y": [i for i in alpha],
-        "x": [str(i) for i in range(8)],
-        "color_continuous_scale": "RdBu" if diverging_scale else "Blues",
-        "color_continuous_midpoint": 0. if diverging_scale else None,
-        "aspect": "equal",
-        **kwargs
-    }
-    imshow(state, **kwargs)
-
-num_games = 50
-focus_games_int = board_seqs_int[:num_games]
-focus_games_string = board_seqs_string[:num_games]
-
-focus_logits, focus_cache = model.run_with_cache(focus_games_int[:, :-1].to(device))
-focus_logits.shape
-
-full_linear_probe = t.load(OTHELLO_MECHINT_ROOT / "main_linear_probe.pth", map_location=device)
-
 rows = 8
 cols = 8
 options = 3
-assert full_linear_probe.shape == (3, cfg.d_model, rows, cols, options)
-
-black_to_play_index = 0
-white_to_play_index = 1
-blank_index = 0
-their_index = 1
-my_index = 2
-
-# Creating values for linear probe (converting the "black/white to play" notation into "me/them to play")
-linear_probe = t.zeros(cfg.d_model, rows, cols, options, device=device)
-linear_probe[..., blank_index] = 0.5 * (full_linear_probe[black_to_play_index, ..., 0] + full_linear_probe[white_to_play_index, ..., 0])
-linear_probe[..., their_index] = 0.5 * (full_linear_probe[black_to_play_index, ..., 1] + full_linear_probe[white_to_play_index, ..., 2])
-linear_probe[..., my_index] = 0.5 * (full_linear_probe[black_to_play_index, ..., 2] + full_linear_probe[white_to_play_index, ..., 1])
-
-layer = 6
-game_index = 0
-move = 29
 
 BLANK = 0
 FILLED_UNAFFECTED = 1
