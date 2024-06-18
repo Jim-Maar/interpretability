16df10ff6a1ae55d681147afe7477213c2ca09e7
Jim Maar <jimmaar1@Gmail.com>
2024-06-18 19:19:34 +0200

    detailed probe evaluation

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import sys
 from pathlib import Path
 import numpy as np
 
+import torch as t
+
 
 # os.chdir(section_dir)
 section_dir = Path.cwd()
@@ -16,7 +18,9 @@ OTHELLO_MECHINT_ROOT = (OTHELLO_ROOT / "mechanistic_interpretability").resolve()
 
 sys.path.append(str(OTHELLO_MECHINT_ROOT))
 from mech_interp_othello_utils import (
-    OthelloBoardState
+    OthelloBoardState,
+    to_int,
+    to_string
 )
 
 # Load Model
@@ -98,3 +102,35 @@ def plot_game(games_str, game_index=0, end_move=16):
         y = [i for i in alpha],
     )
 
+def square_to_tuple(square, is_int=False):
+    if is_int:
+        square = to_string(square)
+    row = square // 8
+    col = square % 8
+    return (row, col)
+
+def to_board_label(i):
+    return f"{alpha[i//8]}{i%8}"
+
+def get_focus_games(model = None, device = "cpu"):
+    # Load board data as ints (i.e. 0 to 60)
+    board_seqs_int = t.tensor(np.load(OTHELLO_MECHINT_ROOT / "board_seqs_int_small.npy"), dtype=t.long)
+    # Load board data as "strings" (i.e. 0 to 63 with middle squares skipped out)
+    board_seqs_string = t.tensor(np.load(OTHELLO_MECHINT_ROOT / "board_seqs_string_small.npy"), dtype=t.long)
+
+    assert all([middle_sq not in board_seqs_string for middle_sq in [27, 28, 35, 36]])
+    assert board_seqs_int.max() == 60
+
+    num_games, length_of_game = board_seqs_int.shape
+    start = 30000
+    num_games = 50
+    focus_games_int = board_seqs_int[start : start + num_games]
+    focus_games_string = board_seqs_string[start: start + num_games]
+
+    if model is not None:
+        focus_logits, focus_cache = model.run_with_cache(focus_games_int[:, :-1].to(device))
+        return focus_games_int, focus_games_string, focus_logits, focus_cache
+    return focus_games_int, focus_games_string
+
+def square_tuple_from_square(square : str):
+    return (alpha.index(square[0]), int(square[1]))
