b49be705e60923644d46741928369bf2047e31c2
Jim Maar <jimmaar1@Gmail.com>
2024-08-14 09:14:31 +0000

    neuron rule extraction works well but inefficient

---
+++ logical_rule_extraction/evaluation.py
@@ -0,0 +1,4 @@
+from interfaces import EvaluaterInterface
+"""
+This File contains the implementation for the evaluation of the logical rule extraction
+"""
---
+++ logical_rule_extraction/experiments.py
@@ -0,0 +1,7 @@
+"""
+This File runs the experiments for the logical rule extraction
+"""
+from interfaces import Experimenter
+# TODO: import the specific implementation of TrainerInterface, ModelInterface, EvaluaterInterface, ArgsInterface
+
+# The Experimenter Class Assumes that the Trainer trains over all Neurons, but I can put this into arguments
