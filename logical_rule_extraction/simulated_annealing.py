11e5a7be857b2b6f46e9eb78db52c5db89d94072
Jim Maar <jimmaar1@Gmail.com>
2024-08-14 09:14:31 +0000

    neuron rule extraction works well but inefficient

---
+++ logical_rule_extraction/simulated_annealing.py
@@ -0,0 +1,4 @@
+"""
+This File contains the implementation for the Simulated Annealing Technique for the logical rule extraction
+TODO: Implement the Simulated Annealing Technique for the logical rule extraction
+"""
